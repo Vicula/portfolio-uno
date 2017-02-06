52d0fbe919443ac90a5cca8b4e7604000a6244c9
Victor Carpenter <victorcarpenter123@gmail.com>
2017-02-06 10:13:13 -0500

    adding some javascript right now

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const panel1 = document.querySelector('.panel-1');
+const panel2 = document.querySelector('.panel-2');
+const panel3 = document.querySelector('.panel-3');
+const panel4 = document.querySelector('.panel-4');
+const panel5 = document.querySelector('.panel-5');
+
+console.log(panel1,panel2,panel3,panel4,panel5)
+
+
+panel1.addEventListener('click', function(){
+  console.log('hey im 1')
+})
+panel2.addEventListener('click', function(){
+  console.log('hey im 1')
+})
+panel3.addEventListener('click', function(){
+  console.log('hey im 1')
+})
+panel4.addEventListener('click', function(){
+  console.log('hey im 1')
+})
+panel5.addEventListener('click', function(){
+  console.log('hey im 1')
+})
