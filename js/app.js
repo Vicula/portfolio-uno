55e9ca634aea39ef13ef4c3848e329019e86886c
Victor Carpenter <victorcarpenter123@gmail.com>
2017-02-06 10:18:43 -0500

    got the javascript to switch between 'home' and sel 1-5

--- js/app.js
+++ js/app.js
@@ -4,21 +4,45 @@ const panel3 = document.querySelector('.panel-3');
 const panel4 = document.querySelector('.panel-4');
 const panel5 = document.querySelector('.panel-5');
 
+const app = document.querySelector('#app-container')
+
 console.log(panel1,panel2,panel3,panel4,panel5)
+
+var crntScreen = 'home'
 
 
 panel1.addEventListener('click', function(){
-  console.log('hey im 1')
+  if(app.className === "sel-1"){
+    app.className = 'home'
+  }else {
+    app.className = 'sel-1'
+  }
 })
 panel2.addEventListener('click', function(){
-  console.log('hey im 1')
+  if(app.className === "sel-2"){
+    app.className = 'home'
+  }else {
+    app.className = 'sel-2'
+  }
 })
 panel3.addEventListener('click', function(){
-  console.log('hey im 1')
+  if(app.className === "sel-3"){
+    app.className = 'home'
+  }else {
+    app.className = 'sel-3'
+  }
 })
 panel4.addEventListener('click', function(){
-  console.log('hey im 1')
+  if(app.className === "sel-4"){
+    app.className = 'home'
+  }else {
+    app.className = 'sel-4'
+  }
 })
 panel5.addEventListener('click', function(){
-  console.log('hey im 1')
+  if(app.className === "sel-5"){
+    app.className = 'home'
+  }else {
+    app.className = 'sel-5'
+  }
 })
